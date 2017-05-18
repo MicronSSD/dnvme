f90053012d80aefb5de16dbbad497b4e91d51107
Will Marone <wmarone@micron.com>
2017-05-18 16:32:01 -0700

    Parameter check and cleanup
    * Added a parameter check to package.sh to annoy people who forget to include
      the version.
    * Added a final step that uses git to purge build products.

--- package.sh
+++ package.sh
@@ -1,6 +1,11 @@
-#!/bin/sh -x
+#!/bin/sh
+if [ -z $1 ]; then
+    echo "Need a version!"
+    exit -1
+fi
 CWD=`pwd`
 cd ..
 tar -czf dnvme_$1.orig.tar.gz --exclude=debian --exclude-vcs dnvme
 cd $CWD
 dpkg-buildpackage -uc -us
+git clean -xdf
